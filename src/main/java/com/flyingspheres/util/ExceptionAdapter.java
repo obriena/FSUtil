58a7224207e1b43963b0f862db595dca35e01046
Aaron OBrien <and_ios@yahoo.com>
2011-11-16 21:42:09 -0600

    Minor documentation

--- src/main/java/com/flyingspheres/util/ExceptionAdapter.java
+++ src/main/java/com/flyingspheres/util/ExceptionAdapter.java
@@ -5,13 +5,15 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 
 public class ExceptionAdapter {
+	/**
+	 * convert an exception into a string for easier logging
+	 */
 	public static String convertThrowable(Exception e) {
         OutputStream os = new ByteArrayOutputStream();
         PrintWriter writer= new PrintWriter(os);
         e.printStackTrace(writer);
         writer.flush();
-        String myMessage = new String(
-                        ((ByteArrayOutputStream)os).toByteArray());
+        String myMessage = new String( ((ByteArrayOutputStream)os).toByteArray());
         return myMessage;
 	}
 }
